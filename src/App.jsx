07d52aaf2654697bc6f35c5ac3aa61bbd3ec564a
faizanKhan <faizankhan46849@gmail.com>
2026-01-23 21:46:32 +0530

    routes secure based on user data in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import LogIn from "./pages/LogIn"
 import SignUp from "./pages/SignUp"
 import { Home } from "./pages/Home"
 import ForgotPassword from "./pages/ForgotPassword"
 import useGetCurrentUser from "./hooks/useGetCurrentUser"
+import { useSelector } from "react-redux"
 export const serverUrl = "http://localhost:8000"
 
 function App() {
   useGetCurrentUser()
+  const {userData} = useSelector(state=>state.user);
   return (
     <>
      <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/signup" element={<SignUp/>} />
-      <Route path="/login" element={<LogIn/>} />
-      <Route path="/forgot-password" element={<ForgotPassword/>} />
+      <Route path="/" element={userData?<Home/>:<Navigate to={"/login"}/>} />
+      <Route path="/signup" element={!userData?<SignUp/>: <Navigate to={"/"}/>} />
+      <Route path="/login" element={!userData?<LogIn/>: <Navigate to={"/"}/>} />
+      <Route path="/forgot-password" element={!userData?<ForgotPassword/>: <Navigate to={'/'}/>} />
      </Routes>
     </>
   )
