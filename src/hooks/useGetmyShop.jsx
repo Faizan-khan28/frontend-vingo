d6f24b602344bf5865f82899563485b2b201131d
faizanKhan <faizankhan46849@gmail.com>
2026-02-08 16:10:35 +0530

    add useGetMyShop and owner slice

---
+++ src/hooks/useGetmyShop.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { serverUrl } from "../App";
+import { useDispatch } from "react-redux";
+import { setuserData } from "../store/userSlice";
+
+function useGetMyShop () {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchShop = async () => {
+      try {
+        const result = await axios.get(`${serverUrl}/api/shop/get-myshop`, {
+          withCredentials: true,
+        });
+        dispatch(setuserData(result.data))
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchShop();
+  }, []);
+}
+
+export default useGetMyShop;
