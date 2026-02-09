0914d80fa6b736f6963f48a2f8946190348da640
faizanKhan <faizankhan46849@gmail.com>
2026-02-09 16:33:47 +0530

    changes in owner dashboard

--- src/components/OwnerDashboard.jsx
+++ src/components/OwnerDashboard.jsx
@@ -8,12 +8,13 @@ export const OwnerDashboard = () => {
     <div>
       <Navbar />
       {!myShopData && 
-      <div className="flex justify-center items-center p-4 sm:p6">
-        <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p6 border-gray-100 hover:shadow-xl transition-shadow duration-300">
+      <div className="flex justify-center items-center p-4 sm:p-6">
+        <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-6 border-gray-100 hover:shadow-xl transition-shadow duration-300">
           <div className="flex flex-col items-center text-center">
-            <FaUtensils className="text-[#ff4d2d] pt-2 w-16 h-16 sm:w-20 sm:h-20 mb-4"  />
+            <FaUtensils className="text-[#ff4d2d] w-16 h-16 sm:w-20 sm:h-20 mb-4"  />
             <h2 className="text-xl sm:text-2xl font-bold text-gray-800 mb-2">Add Your Restaurant</h2>
-            <p>Join Your Food Delivary Platform and Reach Thousands of Hungry Custumers Every Day.</p>
+            <p className="text-gray-600 mb-4 text-sm sm:text-base">Join Your Food Delivary Platform and Reach Thousands of Hungry Custumers Every Day.</p>
+            <button className="bg-[#ff4d2d] text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-600 transition-colors duration-200 cursor-pointer">Get Started</button>
           </div>
         </div>
       </div>}
